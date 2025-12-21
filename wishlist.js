9464fbae458970d6868fdae0ba283e292470497e
Sog-05 <hieupt05.work@gmail.com>
2025-12-21 11:31:33 +0700

    update

--- wishlist.js
+++ wishlist.js
@@ -131,4 +131,9 @@
   window.WishlistRemove = remove;
   window.WishlistOpen = open;
   window.WishlistClose = close;
+  
+  window.toggleWishlist = function(id) {
+    add(id);
+    open(); // Mở drawer sau khi thêm (nếu bạn muốn)
+};
 })();
